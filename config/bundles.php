4abe9d29fe551783d11081da7ce4a0f5d7012253
AnoneWk <anonewk0@gmail.com>
2022-10-25 20:15:19 +0200

    ok

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
